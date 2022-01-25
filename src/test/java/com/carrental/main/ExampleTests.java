851ece1e67584e6280d365108dc73b7e3c0930fb
Adriana Baldacchino <baldacchino.adriana@gmail.com>
2022-01-25 10:13:27 +0100

    Added junit and jacoco dependencies and added an example JUnit test

---
+++ src/test/java/com/carrental/main/ExampleTests.java
@@ -0,0 +1,12 @@
+package com.carrental.main;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTests {
+
+    @Test
+    public void emptyTest(){
+        Assertions.assertEquals(3,3);
+    }
+}

7fcaa60d668fe7165f2fde36a9e46a6b796fa11d
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-01-25 20:21:44 +0100

    Added new microservice, customer management

---
+++ carrental-customer-management/src/main/java/com.um.carrental.customermanagement/Main.java
@@ -0,0 +1,9 @@
+package com.um.carrental.customermanagement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Testing another package.");
+    }
+}

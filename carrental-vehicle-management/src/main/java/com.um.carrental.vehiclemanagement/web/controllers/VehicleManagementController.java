f9f848cc2c3fe2f30c291981ee9712d999f508be
Adriana Baldacchino <baldacchino.adriana@gmail.com>
2022-02-11 10:14:32 +0100

    Added Controller tests

--- carrental-vehicle-management/src/main/java/com.um.carrental.vehiclemanagement/web/controllers/VehicleManagementController.java
+++ carrental-vehicle-management/src/main/java/com.um.carrental.vehiclemanagement/web/controllers/VehicleManagementController.java
@@ -3,18 +3,17 @@ package com.um.carrental.vehiclemanagement.web.controllers;
 import com.um.carrental.vehiclemanagement.services.VehicleManagementService;
 import com.um.carrental.vehiclemanagement.services.VehicleSubmission;
 import com.um.carrental.vehiclemanagement.web.requests.AddVehicleRequest;
+import com.um.carrental.vehiclemanagement.web.requests.DeleteVehicleRequest;
 import com.um.carrental.vehiclemanagement.web.responses.AddVehicleResponse;
+import com.um.carrental.vehiclemanagement.web.responses.DeleteVehicleResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class AddVehicleController {
+public class VehicleManagementController {
 
     @Autowired
     VehicleManagementService vehicleManagementService;
@@ -32,5 +31,15 @@ public class AddVehicleController {
         VehicleSubmission vehicleSubmission = mapper.map(request, VehicleSubmission.class);
 
         return new AddVehicleResponse(vehicleManagementService.addVehicle(vehicleSubmission));
+    }
+
+    // Delete a vehicle
+    // Method --> DELETE
+    // Request --> DeleteVehicleRequest
+    // Response --> 200 (OK), DeleteVehicleResponse
+    @DeleteMapping(value = "vehicles", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public DeleteVehicleResponse deleteVehicle(@RequestBody DeleteVehicleRequest request){
+        return new DeleteVehicleResponse(vehicleManagementService.deleteVehicle(request.getNumberPlate()));
     }
 }
---
+++ carrental-vehicle-management/src/test/java/com.um.carrental.vehiclemanagement/web/VehicleManagementControllerTests.java
@@ -0,0 +1,138 @@
+package com.um.carrental.vehiclemanagement.web;
+
+import com.um.carrental.vehiclemanagement.enums.VehicleType;
+import com.um.carrental.vehiclemanagement.services.VehicleManagementService;
+import com.um.carrental.vehiclemanagement.services.VehicleSubmission;
+import com.um.carrental.vehiclemanagement.web.controllers.VehicleManagementController;
+import com.um.carrental.vehiclemanagement.web.requests.AddVehicleRequest;
+import com.um.carrental.vehiclemanagement.web.requests.DeleteVehicleRequest;
+import com.um.carrental.vehiclemanagement.web.responses.AddVehicleResponse;
+import com.um.carrental.vehiclemanagement.web.responses.DeleteVehicleResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class VehicleManagementControllerTests {
+    @Autowired
+    VehicleManagementController vehicleManagementController;
+
+    @MockBean
+    VehicleManagementService vehicleManagementServiceMock;
+
+    @Test
+    public void testAddCommercialVehicle(){
+        // Setup
+
+        AddVehicleRequest request =
+                new AddVehicleRequest("ABC123", VehicleType.COMMERCIAL);
+        boolean expectedEntered = true;
+        when(vehicleManagementServiceMock.addVehicle(any(VehicleSubmission.class))).thenReturn(expectedEntered);
+
+        // Exercise
+        AddVehicleResponse actualResponse = vehicleManagementController.addVehicle(request);
+
+        // Verify
+
+        assertNotNull(actualResponse, "Response is null.");
+        assertEquals(expectedEntered, actualResponse.isEntered());
+
+        verify(vehicleManagementServiceMock, times(1)).addVehicle(any(VehicleSubmission.class));
+        // Teardown - no teardown stage
+    }
+
+    @Test
+    public void testAddFamilyVehicle(){
+        // Setup
+
+        AddVehicleRequest request =
+                new AddVehicleRequest("ABC123", VehicleType.FAMILY);
+        boolean expectedEntered = true;
+        when(vehicleManagementServiceMock.addVehicle(any(VehicleSubmission.class))).thenReturn(expectedEntered);
+
+        // Exercise
+        AddVehicleResponse actualResponse = vehicleManagementController.addVehicle(request);
+
+        // Verify
+
+        assertNotNull(actualResponse, "Response is null.");
+        assertEquals(expectedEntered, actualResponse.isEntered());
+
+        verify(vehicleManagementServiceMock, times(1)).addVehicle(any(VehicleSubmission.class));
+        // Teardown - no teardown stage
+    }
+
+    @Test
+    public void testAddMotorcycleVehicle(){
+        // Setup
+
+        AddVehicleRequest request =
+                new AddVehicleRequest("ABC123", VehicleType.MOTORCYCLE);
+        boolean expectedEntered = true;
+        when(vehicleManagementServiceMock.addVehicle(any(VehicleSubmission.class))).thenReturn(expectedEntered);
+
+        // Exercise
+        AddVehicleResponse actualResponse = vehicleManagementController.addVehicle(request);
+
+        // Verify
+
+        assertNotNull(actualResponse, "Response is null.");
+        assertEquals(expectedEntered, actualResponse.isEntered());
+
+        verify(vehicleManagementServiceMock, times(1)).addVehicle(any(VehicleSubmission.class));
+        // Teardown - no teardown stage
+    }
+
+    @Test
+    public void testDeletePresentVehicle(){
+        // Setup
+        String numberPlate = "ABC123";
+        AddVehicleRequest addRequest =
+                new AddVehicleRequest(numberPlate, VehicleType.COMMERCIAL);
+        vehicleManagementController.addVehicle(addRequest);
+        DeleteVehicleRequest deleteRequest =
+                new DeleteVehicleRequest(numberPlate);
+        boolean expectedFound = true;
+        when(vehicleManagementServiceMock.deleteVehicle(numberPlate)).thenReturn(expectedFound);
+        // Exercise
+
+        DeleteVehicleResponse actualResponse = vehicleManagementController.deleteVehicle(deleteRequest);
+
+        // Verify
+
+        assertNotNull(actualResponse, "Response is null.");
+        assertEquals(expectedFound, actualResponse.isFound());
+
+        verify(vehicleManagementServiceMock, times(1)).deleteVehicle(numberPlate);
+        // Teardown - no teardown stage
+    }
+
+    @Test
+    public void testDeleteNotPresentVehicle(){
+        // Setup
+        String numberPlate = "ABC123";
+        DeleteVehicleRequest deleteRequest =
+                new DeleteVehicleRequest(numberPlate);
+        boolean expectedFound = false;
+        when(vehicleManagementServiceMock.deleteVehicle(numberPlate)).thenReturn(expectedFound);
+        // Exercise
+
+        DeleteVehicleResponse actualResponse = vehicleManagementController.deleteVehicle(deleteRequest);
+
+        // Verify
+
+        assertNotNull(actualResponse, "Response is null.");
+        assertEquals(expectedFound, actualResponse.isFound());
+
+        verify(vehicleManagementServiceMock, times(1)).deleteVehicle(numberPlate);
+        // Teardown - no teardown stage
+    }
+}

460d24513c44c173ef2c1ea426f3b25ee53b8a58
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-02-17 11:46:12 +0100

    Added call to the repository layer with bug: surname is stored as null

--- carrental-customers-management/src/main/java/com/um/carrental/customer_management/services/AddCustomerService.java
+++ carrental-customers-management/src/main/java/com/um/carrental/customer_management/services/AddCustomerService.java
@@ -1,8 +1,9 @@
 package com.um.carrental.customer_management.services;
 
-import com.um.carrental.customer_management.services.models.AddCustomerSubmission;
+import com.um.carrental.customer_management.data.entities.CustomerEntity;
+import com.um.carrental.customer_management.data.repo.AddCustomerRepository;
+import com.um.carrental.customer_management.services.models.CustomerSubmission;
 import com.um.carrental.customer_management.services.models.Customer;
-import com.um.carrental.customer_management.services.models.CustomerDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.modelmapper.ModelMapper;
@@ -13,11 +14,21 @@ public class AddCustomerService {
     @Autowired
     ModelMapper mapper;
 
-    public Customer addCustomer(AddCustomerSubmission addCustomerSubmission){
-        Customer customer = mapper.map(addCustomerSubmission, Customer.class);
+    @Autowired
+    AddCustomerRepository repository;
 
-        return customer;
+    public Customer addCustomer(CustomerSubmission customerSubmission){
+        Customer customer = mapper.map(customerSubmission, Customer.class);
+
+        // call repo layer to save data
+        CustomerEntity customerEntity = mapper.map(customer, CustomerEntity.class);
+        CustomerEntity savedEntity = repository.save(customerEntity);
+
+        Customer savedCustomer = mapper.map(savedEntity, Customer.class);
+        //return customer;
+        return savedCustomer;
     }
+
 
 
 

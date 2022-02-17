5a499f33c7b4901e67d4186bd5548b67de3c0aa2
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-02-17 11:48:58 +0100

    Removed unneccesarry comment

--- carrental-customers-management/src/main/java/com/um/carrental/customer_management/services/AddCustomerService.java
+++ carrental-customers-management/src/main/java/com/um/carrental/customer_management/services/AddCustomerService.java
@@ -4,13 +4,13 @@ import com.um.carrental.customer_management.data.entities.CustomerEntity;
 import com.um.carrental.customer_management.data.repo.AddCustomerRepository;
 import com.um.carrental.customer_management.services.models.CustomerSubmission;
 import com.um.carrental.customer_management.services.models.Customer;
+import org.hibernate.hql.spi.id.AbstractIdsBulkIdHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.modelmapper.ModelMapper;
 
 @Service
 public class AddCustomerService {
-
     @Autowired
     ModelMapper mapper;
 
@@ -25,12 +25,7 @@ public class AddCustomerService {
         CustomerEntity savedEntity = repository.save(customerEntity);
 
         Customer savedCustomer = mapper.map(savedEntity, Customer.class);
-        //return customer;
+       
         return savedCustomer;
     }
-
-
-
-
-
 }

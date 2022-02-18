48fe0a55a1d1e9d088c8d4aaf075fd9f5d8b2309
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-02-18 13:10:10 +0100

    Added new methods, changed import statement

--- carrental-customers-management/src/main/java/com/um/carrental/customer_management/web/responses/GetCustomerResponse.java
+++ carrental-customers-management/src/main/java/com/um/carrental/customer_management/web/responses/GetCustomerResponse.java
@@ -1,6 +1,6 @@
 package com.um.carrental.customer_management.web.responses;
 
-import com.um.carrental.customer_management.web.requests.CustomerDetails;
+import com.um.carrental.customer_management.services.models.CustomerDetails;
 
 import java.util.List;
 
@@ -15,5 +15,25 @@ public class GetCustomerResponse {
     }
     public GetCustomerResponse(){
 
+    }
+
+    public GetCustomerResponse(String customerId){
+        this.customerId = customerId;
+    }
+
+    public GetCustomerResponse(List<CustomerDetails> customerDetails){
+        this.customerDetails = customerDetails;
+    }
+
+    public String getCustomerId(){
+        return customerId;
+    }
+
+    public List<CustomerDetails> getCustomerDetails() {
+        return customerDetails;
+    }
+
+    public void setCustomerDetails(List<CustomerDetails> customerDetails){
+        this.customerDetails = customerDetails;
     }
 }

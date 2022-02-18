7e4ca864909c0a6471fcf230fce5b1f0ecb90acf
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-02-18 23:09:38 +0100

    Testing service layer

---
+++ carrental-customers-management/src/test/java/com/um/carrental/customer_management/services/CustomerServiceTests.java
@@ -0,0 +1,55 @@
+package com.um.carrental.customer_management.services;
+
+import com.um.carrental.customer_management.services.models.CustomerSubmission;
+import com.um.carrental.customer_management.web.controllers.CustomerController;
+import com.um.carrental.customer_management.web.requests.AddCustomerRequest;
+import com.um.carrental.customer_management.web.requests.CustomerDetails;
+import com.um.carrental.customer_management.web.responses.SubmitCustomerResponse;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class CustomerServiceTests {
+
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    ModelMapper mapper = new ModelMapper();
+
+    @MockBean
+    AddCustomerService customerServiceMock;
+
+    @Test
+    public void testAddCustomer(){
+        // Setup
+        AddCustomerRequest request = new AddCustomerRequest(List.of(new CustomerDetails("andrew borg", 73)));
+        String customerId = UUID.randomUUID().toString();
+        when(customerServiceMock.addCustomer(any(CustomerSubmission.class))).thenReturn(customerId);
+        // Exercise
+        SubmitCustomerResponse actualResponse = customerController.submit(customerId, request);
+        // Verify
+        assertNotNull(actualResponse, "Response is null");
+        assertEquals(customerId, actualResponse.getId());
+    }
+
+    @Test
+    public void testDeleteCustomer(){
+                
+    }
+    // public void testGetValidCustomer()
+    // public void testDeleteNonExistingCustomer()
+}

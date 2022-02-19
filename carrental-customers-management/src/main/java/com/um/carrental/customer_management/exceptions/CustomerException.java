9add648551c38f7f64ee722e4fcd2659c0874ba9
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-02-19 09:45:54 +0100

    Added custom exception handler for GET end point

---
+++ carrental-customers-management/src/main/java/com/um/carrental/customer_management/exceptions/CustomerException.java
@@ -0,0 +1,15 @@
+package com.um.carrental.customer_management.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.NoSuchElementException;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason="No customer exists by this ID")
+public class CustomerException extends NoSuchElementException {
+
+    public CustomerException(){
+        super();
+    }
+}

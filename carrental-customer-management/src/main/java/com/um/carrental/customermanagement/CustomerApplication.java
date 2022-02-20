259892a3f40316bf3c51ddbb9d41c83a86afc50c
marietta112 <92716430+marietta112@users.noreply.github.com>
2022-02-20 20:18:18 +0100

    Removed extra directory

--- carrental-customer-management/src/main/java/com/um/carrental/customermanagement/CustomerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.um.carrental.customermanagement;
-
-public class CustomerApplication {
-    public static void main(String[] args) throws InterruptedException{
-        System.out.println("Hello World!");
-        System.out.println("Testing another package.");
-       // ConfigurableApplicationContext context = SpringApplication.run(CustomerApplication.class, args);
-    }
-}
--- carrental-customer-management/src/main/java/customermanagement2/Main.java
+++
@@ -1,9 +0,0 @@
-package customermanagement2;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Testing another package.");
-    }
-}
